5bb6afa1301117e542c3c1e117c34cf1a7a80978
Bloom <bloomtauta@gmail.com>
2023-02-18 12:50:09 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
-
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+   
+end
+book = Book.new("And There Were None")
+# book.title
+# book.author_name = ("Agatha Christie")
+book.turn_page
+# binding.pry
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @conditiion = condition
+    end
+    def cobble 
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+shoe = Shoe.new("Adidas")
+shoe.brand
+shoe.cobble
+# shoe.condition=("new")
+
+# binding.pry
